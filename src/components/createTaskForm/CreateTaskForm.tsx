9a13a726672fa8a5df9fdd824212498c447ae917
Eduardo Donato <eduardo.donato@gmail.com>
2023-01-25 15:28:41 -0300

    add task selected field

--- src/components/createTaskForm/CreateTaskForm.tsx
+++ src/components/createTaskForm/CreateTaskForm.tsx
@@ -1,11 +1,18 @@
-import { FC, ReactElement } from "react"
+import { FC, ReactElement, useState } from "react"
 import { Box, Stack, Typography } from "@mui/material"
 
 import { TaskTitleField } from "./TaskTitleField"
 import { TaskDescriptionField } from "./TaskDescriptionField"
 import { TaskDateField } from "./TaskDateField"
+import { TaskSelectField } from "./TaskSelectField"
+
+import { Status } from "./enums/Status"
+import { Priority } from "./enums/Priority"
 
 export const CreateTaskForm: FC = (): ReactElement => {
+    const [status, setStatus] = useState("")
+    const [priority, setPriority] = useState("")
+
     return (
         <Box display="flex" flexDirection="column" alignItems="flex-start" width="100%" px={4} my={6}>
             <Typography mb={2} component="h2" variant="h6">
@@ -18,9 +25,11 @@ export const CreateTaskForm: FC = (): ReactElement => {
                 <TaskDateField />
 
                 <Stack sx={{ width: "100%" }} direction="row" spacing={2}>
-                    {/* <TaskSelectField
+                    <TaskSelectField
                         label="Status"
                         name="status"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
                         items={[
                             {
                                 value: Status.todo,
@@ -35,21 +44,23 @@ export const CreateTaskForm: FC = (): ReactElement => {
                     <TaskSelectField
                         label="Priority"
                         name="priority"
+                        value={priority}
+                        onChange={(e) => setPriority(e.target.value)}
                         items={[
                             {
                                 value: Priority.low,
-                                label: Priority.low,
+                                label: Priority.low.toUpperCase(),
                             },
                             {
                                 value: Priority.normal,
-                                label: Priority.normal,
+                                label: Priority.normal.toUpperCase(),
                             },
                             {
                                 value: Priority.high,
-                                label: Priority.high,
+                                label: Priority.high.toUpperCase(),
                             },
                         ]}
-                    /> */}
+                    />
                 </Stack>
             </Stack>
 
